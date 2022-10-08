2a34b96c6fb1bb246aacc6efdcec5dcd49a259cd
ZZJ <2086850087@qq.com>
2022-10-08 17:45:25 +0800

    day02-完成启用、禁用、编辑员工功能

--- src/main/java/com/zzj/reggie/config/WebMvcConfig.java
+++ src/main/java/com/zzj/reggie/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.zzj.reggie.config;
 
+import com.zzj.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * @BelongsProject: reggie_take_out
@@ -22,5 +27,16 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("进行静态资源访问");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    //扩展mvc框架的消息转换器
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //创建消息转换器对象
+        MappingJackson2CborHttpMessageConverter messageConverter = new MappingJackson2CborHttpMessageConverter();
+        //设置对象转换器，底层使用Jackson将java对象转为json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架的容器中
+        converters.add(0, messageConverter);
     }
 }
